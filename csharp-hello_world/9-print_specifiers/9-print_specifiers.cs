6203ddcca0a4886242cbfb975f873bae64b699d4
Repli1 <cladescandor42@gmail.com>
2023-07-26 10:34:27 -0300

    fix precision

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-		Console.WriteLine("Percent: {0:P}", percent);
+		Console.WriteLine("Percent: {0:P2}", percent);
         Console.WriteLine("Currency: {0:C}", currency);
 	}
 }
