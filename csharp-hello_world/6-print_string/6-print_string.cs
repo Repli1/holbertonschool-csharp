57580714956194fe2ac9cbfdcccce96a988290d3
Repli1 <cladescandor42@gmail.com>
2023-07-26 09:33:44 -0300

    Add task 6

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.WriteLine("{0}{0}{0}", str);
+        Console.WriteLine(str.Substring(0, 9));
+        }
+}
