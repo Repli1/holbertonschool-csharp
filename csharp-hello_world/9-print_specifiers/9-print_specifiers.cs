970ddc123c9c733b93ba834bfa13e024b1657063
Repli1 <cladescandor42@gmail.com>
2023-07-26 10:24:57 -0300

    Add task 9

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine("Percent: {0:P}", percent);
+        Console.WriteLine("Currency: {0:C}", currency);
+	}
+}
