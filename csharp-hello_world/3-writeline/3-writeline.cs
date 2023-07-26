ac18aea63941893aba840e912e27966689d90159
Repli1 <cladescandor42@gmail.com>
2023-07-26 09:12:51 -0300

    change .NET version to 2.1

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("\"Programming is like building a multilingual puzzle");
+﻿using System;
+
+namespace _3_writeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }
+    }
+}
