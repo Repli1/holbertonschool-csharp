45264978f8a532e7e68f19b1d8feaf73bfe0772b
Repli1 <cladescandor42@gmail.com>
2023-07-26 09:41:04 -0300

    Add task 8

---
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "Street";
+        string str2 = "Mission";
+        int int1 = 972;
+        Console.WriteLine("{0} {1} {2}", int1, str2, str1);
+    }
+}
