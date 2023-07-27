0d5a4f9808b99463592adfb11f45cd60fb586787
Repli1 <cladescandor42@gmail.com>
2023-07-27 03:05:18 -0300

    Add task 5

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = -1;
+            for (int i = 0; i < 99; i++)
+            {
+                if ((i % 10) == 0)
+                {
+                    j++;
+                }
+                Console.Write("{0}{1}, ", j, i % 10);
+            }
+            Console.Write("99");
+        }
+    }
+}
