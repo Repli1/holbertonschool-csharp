22ef2b953767e9883671bb6a1bd0b7d62be06615
Repli1 <cladescandor42@gmail.com>
2023-07-27 08:35:24 -0300

    Add task 8

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        Console.Write(Math.Abs(number % 10));
+        return (Math.Abs(number % 10));
+    }
+}
