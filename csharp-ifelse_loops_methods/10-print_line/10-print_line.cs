22084fb0b4adcac9f583c2c8973b8cb29dc6df49
Repli1 <cladescandor42@gmail.com>
2023-07-27 08:45:24 -0300

    Add task 10

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write("_");
+        }
+        Console.WriteLine("");
+    }
+}
