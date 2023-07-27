3eee0353909a113137de71c9625e655be30074d6
Repli1 <cladescandor42@gmail.com>
2023-07-27 02:20:58 -0300

    Add task 4

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 98; i++)
+            {
+                Console.Write("{0} = 0x{1}\n", i, i.ToString("x"));
+            }
+        }
+    }
+}
