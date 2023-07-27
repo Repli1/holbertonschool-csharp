834aa22f8038962e02ac47de09f2016d52a3a23a
Repli1 <cladescandor42@gmail.com>
2023-07-27 02:12:55 -0300

    Add task 3

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                if (i != 'q' && i != 'e')
+                    Console.Write(i);
+            }
+        }
+    }
+}
