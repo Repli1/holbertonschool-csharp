da248572b302c6884350bbae578053dcf0a95fe4
Repli1 <cladescandor42@gmail.com>
2023-08-05 19:34:49 -0300

    Add task 6

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> newList = new List<bool>();
+        for (int i = 0; i < myList.Count; i++) {
+            if (myList[i] % 2 == 0)
+                newList.Add(true);
+            else
+                newList.Add(false);
+        }
+        return newList;
+    }
+}
