e5cd9c31747f2b71304b942974e747a050567580
Repli1 <cladescandor42@gmail.com>
2023-08-05 20:24:36 -0300

    Add task 8

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int n = 0;
+        for ( int i = 0; i < myDict.Count; i++)
+            n++;
+        return n;
+    }
+}
