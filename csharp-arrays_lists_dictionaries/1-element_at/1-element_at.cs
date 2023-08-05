63b3bbfbc9ea9aee92a5bc8e534b7fd1b8a0c51c
Repli1 <cladescandor42@gmail.com>
2023-08-05 18:58:37 -0300

    Add task 1

---
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        if (array.Length <= index || index < 0) {
+            Console.WriteLine("Index out of range");
+            return (-1);
+        }
+        return(array[index]);
+    }
+}
