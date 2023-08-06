1fb8e68a299c329d8e97a853ce19eed5646d74fb
Repli1 <cladescandor42@gmail.com>
2023-08-06 01:18:35 -0300

    add task 5

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList <int> llist = new LinkedList<int>();
+        if (size < 0)
+            return llist;
+        for (int i = 0; i < size; i++)
+        {
+            llist.AddLast(i);
+            Console.WriteLine(i);
+        }
+        return llist;
+    }
+}
