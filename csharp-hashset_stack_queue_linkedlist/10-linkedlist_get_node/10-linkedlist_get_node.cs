d94e14527485a3c3bd067d7cf87da382e6b8c773
Repli1 <cladescandor42@gmail.com>
2023-08-06 03:01:20 -0300

    Add task 10

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int i = 0;
+        foreach (int node in myLList)
+        {
+            if (i == n)
+                return node;
+            i++;
+        }
+        return 0;
+    }
+}
