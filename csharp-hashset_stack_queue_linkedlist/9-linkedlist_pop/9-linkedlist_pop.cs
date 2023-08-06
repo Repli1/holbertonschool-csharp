ef65a35d76e9544fb89672a6090b17c5af76fcbc
Repli1 <cladescandor42@gmail.com>
2023-08-06 02:53:57 -0300

    Add empty list edgecase

--- csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -5,6 +5,8 @@ class LList
 {
     public static int Pop(LinkedList<int> myLList)
     {
+        if (myLList.Count == 0)
+            return 0;
         int n = myLList.First.Value;
         myLList.RemoveFirst();
         return n;
