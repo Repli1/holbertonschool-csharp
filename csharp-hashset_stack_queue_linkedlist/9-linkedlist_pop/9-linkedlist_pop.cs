4d9f066f9bf9c104680e5f200ec2e8a7494f2aa5
Repli1 <cladescandor42@gmail.com>
2023-08-06 02:51:29 -0300

    Add task 9

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int n = myLList.First.Value;
+        myLList.RemoveFirst();
+        return n;
+    }
+}
