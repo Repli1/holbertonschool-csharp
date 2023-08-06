b0728656131cfb495eea9ff804f7a54ddc8390f7
Repli1 <cladescandor42@gmail.com>
2023-08-06 02:32:23 -0300

    Add task 8

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int i = 0;
+        foreach (int n in myLList)
+        {
+            if (n == value)
+                return i;
+            i++;
+        }
+        return -1;
+    }
+}
