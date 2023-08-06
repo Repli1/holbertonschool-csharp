23a5c22eb943156a0434668598ae6c878562adab
Repli1 <cladescandor42@gmail.com>
2023-08-05 21:23:55 -0300

    Add task 15

---
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int rows = myMatrix.GetLength(0);
+        int columns = myMatrix.GetLength(1);
+        int[,] newMatrix = new int[rows, columns];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                newMatrix[i, j] = (myMatrix[i, j] * myMatrix[i, j]);
+            }
+        }
+        return newMatrix;
+    }
+}
