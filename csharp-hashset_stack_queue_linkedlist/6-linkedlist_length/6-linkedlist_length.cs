923d60c7e7a903b1536d439c451f0606abc443df
Repli1 <cladescandor42@gmail.com>
2023-08-06 01:22:37 -0300

    Add task 6

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int i = 0;
+        foreach (int n in myLList)
+        {
+            i++;
+        }
+        return i;
+    }
+}
