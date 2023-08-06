cc961fdb7b3a09ca68707978ddbbb68532e3180d
Repli1 <cladescandor42@gmail.com>
2023-08-06 03:11:10 -0300

    Add task 11

---
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int sum = 0;
+        foreach (int n in myLList)
+            sum += n;
+        return sum;
+    }
+}
