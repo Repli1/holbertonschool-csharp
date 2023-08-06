adcf61655e3d927901fba6931ce873f267478a6a
Repli1 <cladescandor42@gmail.com>
2023-08-06 00:00:07 -0300

    Add task 0

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int sum = 0;
+        List<int> newList = new List<int>();
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (newList.Contains(myList[i]) == false)
+                newList.Add(myList[i]);
+        }
+        for (int i = 0; i < newList.Count; i++)
+            sum += newList[i];
+        return sum;
+    }
+}
