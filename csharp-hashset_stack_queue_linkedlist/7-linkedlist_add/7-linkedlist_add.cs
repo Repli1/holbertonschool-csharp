699d932dfff0c2789e612c56146975b282086f9e
Repli1 <cladescandor42@gmail.com>
2023-08-06 02:25:52 -0300

    Add task 7

---
+++ csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        myLList.AddFirst(n);
+        return myLList.First;
+    }
+}
