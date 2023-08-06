3070f770b0fb73cceb7b2963830c4a7544542b7d
Repli1 <cladescandor42@gmail.com>
2023-08-06 04:08:06 -0300

    Add task 16

---
+++ csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[][] jaggedArray = new int[3][];
+        jaggedArray[0] = new int[] { 0, 1, 2, 3 };
+        jaggedArray[1] = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+        jaggedArray[2] = new int[] { 0, 1 };
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < jaggedArray[i].Length; j++)
+            {
+                Console.Write(jaggedArray[i][j]);
+                if (j != (jaggedArray[i].Length - 1))
+                    Console.Write(" ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
