5cf07cbea4624df31b2b35cf3dce843342855688
Repli1 <cladescandor42@gmail.com>
2023-08-05 21:14:48 -0300

    Add task 14

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = new int[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    myArray[i, j] = 0;
+                }
+            }
+            myArray[2, 2] = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(myArray[i, j]);
+                    if (j != 4)
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
