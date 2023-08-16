90a486631de6fe84e01dd411aaf890173d78e54a
Repli1 <cladescandor42@gmail.com>
2023-08-16 20:03:51 -0300

    Add task 2

---
+++ csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string Name, float Age, string Owner, Rating Rating)
+    {
+        name = Name;
+        age = Age;
+        owner = Owner;
+        rating = Rating;
+    }
+}
