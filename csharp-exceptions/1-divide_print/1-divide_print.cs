7b0168db996f1d46db26bc55d5248293806313d4
Repli1 <cladescandor42@gmail.com>
2023-08-16 19:06:53 -0300

    Add task 1

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int c = 0;
+
+        try
+        {
+            c = a / b;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {c}");
+        }
+    }
+}
