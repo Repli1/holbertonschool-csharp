7bf7107844370caf32efc6e5c6a1da7eac7de26a
Repli1 <cladescandor42@gmail.com>
2023-08-16 18:54:40 -0300

    Add task 0

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int i = 0;
+
+        try
+        {
+            for (; i < n; i++)
+                Console.WriteLine(myList[i]);
+            return i;
+        }
+        catch (Exception ex)
+        {
+            return i;
+        }
+    }
+}
