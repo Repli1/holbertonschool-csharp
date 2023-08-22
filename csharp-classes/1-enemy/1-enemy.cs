2ee85eb59210f2576d58810f6d94f9a7dec74ce6
Repli1 <cladescandor42@gmail.com>
2023-08-22 01:23:46 -0300

    Add task 1

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    ///  Enemies class containing Zombie method
+    /// </summary>
+    class Zombie
+    {
+        public int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+        
+    }
+}
