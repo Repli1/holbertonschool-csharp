e8dd7977d45b3225b45e4e049187e4b1816a0158
Repli1 <cladescandor42@gmail.com>
2023-08-22 01:56:10 -0300

    Add task 5

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    ///  Enemies class containing Zombie method
+    /// </summary>
+    class Zombie
+    {
+        private int health;
+        private string name = "(No name)";
+
+        public Zombie()
+        {
+            health = 0;
+        }
+        
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Zombie Name: {name} / Total Health: {health}";
+        }
+    }
+}
