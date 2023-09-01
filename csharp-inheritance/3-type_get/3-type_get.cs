83d4a005960caa53c528ed721563471c34837d83
Repli1 <cladescandor42@gmail.com>
2023-08-31 23:49:57 -0300

    Add task 3

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+
+class Obj
+{
+    public static void Print(object myObj)
+    {
+        Console.WriteLine($"{myObj.GetType().Name} Properties:");
+        foreach (PropertyInfo property in (myObj.GetType()).GetProperties())
+        {
+            Console.WriteLine(property.Name);
+        }
+        Console.WriteLine($"{myObj.GetType().Name} Methods:");
+        foreach (MethodInfo method in (myObj.GetType()).GetMethods())
+        {
+            Console.WriteLine(method.Name);
+        }
+    }
+}
