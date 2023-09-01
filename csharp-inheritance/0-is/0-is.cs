c9d1ab06dc6da7bc288dc367762b90ff5b904ecb
Repli1 <cladescandor42@gmail.com>
2023-08-31 22:42:05 -0300

    Add task 0

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Obj
+{
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj is int)
+            return true;
+        else 
+            return false;
+    }
+}
