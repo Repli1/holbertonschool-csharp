210d2888b9b269aae2f7963ab20d7dd9960e3c63
Repli1 <cladescandor42@gmail.com>
2023-09-17 22:17:23 -0300

    Add task 3

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length != 2 && vector.Length != 3)
+        {
+            return -1;
+        }
+
+        double sum = 0;
+
+        foreach (double component in vector)
+        {
+            sum += component * component;
+        }
+
+        double result = Math.Sqrt(sum);
+
+        return Math.Round(result, 2);
+    }
+}
