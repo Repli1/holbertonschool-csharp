590fb179d87ae6875b51076e2c91c47851ecb4b5
Repli1 <cladescandor42@gmail.com>
2023-09-27 11:54:01 -0300

    fix2

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Drawing;
+using System.Drawing.Common;
 
 public class ImageProcessor
 {
@@ -7,9 +7,9 @@ public class ImageProcessor
     {
         foreach (var filename in filenames)
         {
-            using (var originalImage = new System.Drawing.Bitmap(filename))
+            using (var originalImage = new Bitmap(filename))
             {
-                using (var invertedImage = new System.Drawing.Bitmap(originalImage.Width, originalImage.Height))
+                using (var invertedImage = new Bitmap(originalImage.Width, originalImage.Height))
                 {
                     for (int x = 0; x < originalImage.Width; x++)
                     {
