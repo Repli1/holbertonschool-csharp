acfb9b16096891714b3ea6a3158ebc2b234cdf64
Repli1 <cladescandor42@gmail.com>
2023-11-12 20:45:35 -0300

    Add task 1

---
+++ csharp-generics/1-enqueue/1-enqueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Queue<T>
+{
+    public class Node
+    {
+        public T Value { get; }
+        public Node Next { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int count;
+    public string CheckType()
+    {
+        return typeof(T).FullName;
+    }
+    public void Enqueue(T item)
+    {
+        Node newNode = new Node(item);
+
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.Next = newNode;
+            tail = newNode;
+        }
+
+        count++;
+    }
+    public int Count()
+    {
+        return count;
+    }
+}
