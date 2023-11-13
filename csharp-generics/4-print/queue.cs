a4e9b61db22e74e240f29224dffdb82850a4358e
Repli1 <cladescandor42@gmail.com>
2023-11-12 21:01:08 -0300

    Add task 4

---
+++ csharp-generics/4-print/queue.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+class Queue<T>
+{
+    public class Node
+    {
+        public T Value { get; }
+        public Node Next { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int count;
+    public string CheckType()
+    {
+        return typeof(T).FullName;
+    }
+    public void Enqueue(T item)
+    {
+        Node newNode = new Node(item);
+
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.Next = newNode;
+            tail = newNode;
+        }
+
+        count++;
+    }
+    public int Count()
+    {
+        return count;
+    }
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+
+        T value = head.Value;
+        head = head.Next;
+
+        if (head == null)
+        {
+            tail = null;
+        }
+
+        count--;
+
+        return value;
+    }
+    public T Peek()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+
+        return head.Value;
+    }
+    public void Print()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return;
+        }
+
+        Node current = head;
+
+        while (current != null)
+        {
+            Console.WriteLine($"{current.Value}");
+            current = current.Next;
+        }
+    }
+}
