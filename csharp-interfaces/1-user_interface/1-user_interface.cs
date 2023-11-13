809372ca15a91ab251311b0b508dadc7cf7ff242
Repli1 <cladescandor42@gmail.com>
2023-11-12 21:59:02 -0300

    Add task 1

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public interface IInteractive
+{
+    void Interact();
+}
+
+public interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+public interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+public abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public void Interact()
+    {
+    }
+
+    public int durability { get; set; }
+    public void Break()
+    {
+    }
+
+    public bool isCollected { get; set; }
+    public void Collect()
+    {
+    }
+}
