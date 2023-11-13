45f42218e58c7314b5e78e3003a7bc68a43d1094
Repli1 <cladescandor42@gmail.com>
2023-11-12 21:51:13 -0300

    Add task 0

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,16 @@
+﻿public abstract class Base
+{
+    public string Name { get; set; }
+
+    public Base(string name)
+    {
+        Name = name;
+    }
+
+    public override string ToString()
+    {
+        string typeName = GetType().Name;
+
+        return $"{Name} is a {typeName}";
+    }
+}
