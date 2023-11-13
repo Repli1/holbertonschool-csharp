27500195ee12a3f767a70acbc46d8c557e289e02
Repli1 <cladescandor42@gmail.com>
2023-11-12 22:09:34 -0300

    Add task 2

---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public interface IInteractive
+{
+    void Interact();
+}
+
+public interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+public interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+public abstract class Base
+{
+    public string name { get; set; }
+
+    public Base(string name = "Door")
+    {
+        this.name = name;
+    }
+
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
+public class Door : Base, IInteractive
+{
+    public Door(string name = "Door") : base(name)
+    {
+    }
+
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
